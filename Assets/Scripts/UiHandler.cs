94f8febb7d35c8186afc1467d090a4b034591564
RaMo-teamhellep <47291672+RaMo-teamhellep@users.noreply.github.com>
2020-02-13 11:52:35 +0100

    Added Canvases for Start, Gameplay and End

---
+++ Assets/Scripts/UiHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiHandler : MonoBehaviour
+{
+    //UI Handler
+
+    /*
+        Script that handles updating of points on ui    
+    */
+
+
+    //References
+    public Canvas GameplayCanvas;
+
+
+    //Functions
+
+}
