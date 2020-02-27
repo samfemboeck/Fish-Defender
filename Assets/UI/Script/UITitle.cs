885b1eb3a4a0ce89634568dcf9af85ac28f65142
RaMo-teamhellep <47291672+RaMo-teamhellep@users.noreply.github.com>
2020-02-27 08:41:36 +0100

    Update UITitle.cs
    - added comments

--- Assets/UI/Script/UITitle.cs
+++ Assets/UI/Script/UITitle.cs
@@ -13,6 +13,9 @@ public class UITitle : MonoBehaviour
     [SerializeField]
     Text fishesJoinedText;
 
+    //Private Var
+    private int currentGamepads = 0;    //RM used for checking whether gamepads have changed
+
     void Start()
     {
         deviceManager = new DeviceManager();
@@ -22,18 +25,20 @@ public class UITitle : MonoBehaviour
     {
         int gamepads = deviceManager.gamepads.Count;
 
+        //Only iterate all UI images if gamepads have changed
+        if (gamepads == currentGamepads)
+            return;
+
+        //Iterate all UI images and check whether they should be visible
         for (int i = 0; i < fishSprites.Length; i++)
         {
             if (i < gamepads)
-            {
                 fishSprites[i].enabled = true;
-            }
             else
-            {
                 fishSprites[i].enabled = false;
-            }
         }
 
+        //Update displayed text
         if (gamepads == 1)
             fishesJoinedText.text = gamepads + " fish joined";
         else
