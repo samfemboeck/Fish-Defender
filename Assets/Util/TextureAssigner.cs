e780c8eeca0a223a5586e39c9d451c538af67203
Philipp <gp@sol.at>
2020-03-02 17:13:03 +0100

    fixed i think
    fixed the correct assignement

--- Assets/Util/TextureAssigner.cs
+++ Assets/Util/TextureAssigner.cs
@@ -5,18 +5,12 @@ using UnityEngine;
 
 public class TextureAssigner : MonoBehaviour
 {
-    static int i;
     MeshRenderer rend;
-    [SerializeField]
-    ColorPool idk;
-    public Color[] colours;
-
-    public void Awake()
+    public void Start()
     {
-        colours = new Color[] { idk.color1, idk.color2, idk.color3, idk.color4, idk.color5, idk.color6, idk.color7, idk.color8 };
         rend = gameObject.GetComponent<MeshRenderer>();
-        rend.material.SetColor("_Color", colours[i]);
-        rend.material.SetColor("_EmissionColor", colours[i]);
-        i++;
+        Color col = gameObject.GetComponentInParent<PlayerColor>().color;
+        rend.material.SetColor("_Color", col);
+        rend.material.SetColor("_EmissionColor", col);
     }
 }
