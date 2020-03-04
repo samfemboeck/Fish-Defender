3f311eb9da99945d6d13b23654e86b892cb2fb90
Philipp <gp@sol.at>
2020-03-04 22:36:18 +0100

    first draft

---
+++ Assets/Util/towerColAssigner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class towerColAssigner : MonoBehaviour
+{
+    MeshRenderer rend;
+    Light light;
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = gameObject.GetComponentInChildren<Light>();
+        rend = gameObject.GetComponent<MeshRenderer>();
+        Color col = gameObject.GetComponent<PlayerColor>().color;
+        rend.material.SetColor("_Color", col);
+        rend.material.SetColor("EmissionColor", col);
+        light.color = col;
+    }
+}
---
+++ Assets/Util/towerMatList.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class towerMatList : MonoBehaviour
+{
+    public Material[] m_Mats = new Material[3];
+}
