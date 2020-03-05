a3c087633acb2db549071781faf6b81a4066b85a
RaMo-teamhellep <RaMo-teamhellep@users.noreply.github.com>
2020-03-05 18:45:17 +0100

    Basic design for end ui

--- Assets/UI/Script/UIEnd.cs
+++ Assets/UI/Script/UIEnd.cs
@@ -1,7 +1,47 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class UIEnd : MonoBehaviour
 {
-    // TODO
+    /*
+     * UI Title
+     * This class handles functionaltiy for the end UI.
+     */
+
+    [SerializeField]
+    Image winnerImage;
+
+    [SerializeField]
+    Sprite fishSprite;
+
+    [SerializeField]
+    Sprite towerSprite;
+
+    [SerializeField]
+    string[] fishQuotes;
+
+    [SerializeField]
+    Text factText;
+
+    [SerializeField]
+    GameObjectSet towerSet;
+
+    [SerializeField]
+    GameObjectSet fishSet;
+
+    private void Start()
+    {
+        //Get winner
+        //Get tower score
+        //int towerScore = towerSet.items[0].GetComponent<TowerScore>().Score;
+        print("end tower score: " + towerSet.items.Count);
+        print("end fish score: " + fishSet.items.Count);
+
+        //Set image
+
+        //Choose random fact
+        int randomIndex = Random.Range(0, fishQuotes.Length - 1);
+        factText.text = fishQuotes[randomIndex];
+    }
 }
