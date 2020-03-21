a190232c66a7b56f75c83d4a318858ab46283f01
RaMo-teamhellep <47291672+RaMo-teamhellep@users.noreply.github.com>
2020-03-21 22:03:37 +0100

    Update StateEnd.cs
    started implementation

--- Assets/State/StateEnd.cs
+++ Assets/State/StateEnd.cs
@@ -15,8 +15,14 @@ public class StateEnd : MonoBehaviour
     [SerializeField]
     GameObjectSet collectibles;
 
+    private GameObject[] winner;
+    private int highestFishScore;
+    
+
     private void Start()
     {
+        winner = new GameObject[fishes.Count];
+
         fishes.RemoveAll();
         towerPlayers.RemoveAll();
         collectibles.RemoveAll();
@@ -28,5 +34,44 @@ public class StateEnd : MonoBehaviour
     public void OnPressSpace(InputAction.CallbackContext ctx)
     {
         ScreenManager.Instance.ChangeToScreen(title);
+    }
+
+    private GameObject[] GetWinner()
+    {
+        GameObject[] winnerFishes;
+
+        int fishScore = GetHighestFishScore(out winnerFishes);
+        int towerScore = towerPlayers.items[0].GetComponent<TowerScore>().Score;
+
+        if (towerScore <= 0 || fishScore >= 10)
+        {
+            return winnerFishes;
+        }
+        else
+        {
+            GameObject[] winnerTower = { towerPlayers.items[0] };
+            return winnerTower;
+        }
+    }
+
+    private int GetHighestFishScore(out GameObject[] winner)
+    {
+        GameObject[] winnerFishes = new GameObject[fishes.Count];
+        int nextIndex = 0;
+        int winnerScore = -1;
+
+        foreach (GameObject fish in fishes.items)
+        {
+            int fishScore = fish.GetComponent<FishScore>().Score;
+            if (fishScore > winnerScore)
+            {
+                winnerFishes[nextIndex] = fish;
+                winnerScore = fishScore;
+                nextIndex++;
+            }
+        }
+
+        winner = winnerFishes;
+        return winnerScore;
     }
 }
