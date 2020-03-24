9a496d3c530e90eac87af0a95e6edf9026840ddb
F L <aims20@me.com>
2020-03-24 23:40:40 +0100

    crude implementation of animation on buttonpress! fish is waddling around now!

--- Assets/Fish/FishController.cs
+++ Assets/Fish/FishController.cs
@@ -6,22 +6,31 @@ using static UnityEngine.InputSystem.InputAction;
 public class FishController : MonoBehaviour
 {
     FishMovement fishMovement;
+    AnimationControl fishAnimation;
+
 
     private void Awake()
     {
         PlayerInput playerInput = GetComponent<PlayerInput>();
         playerInput.playerControls.Gamepad.PressButtonSouth.performed += OnPressButtonSouth;
+        playerInput.playerControls.Gamepad.ReleaseButtonSouth.performed += OnReleaseButtonSouth;
         playerInput.playerControls.Gamepad.MoveLeftStick.performed += OnMoveLeftStick;
     }
 
     private void Start()
     {
         fishMovement = GetComponent<FishMovement>();
+        fishAnimation = GetComponent<AnimationControl>();
     }
 
     private void OnPressButtonSouth(CallbackContext ctx)
     {
         fishMovement.PushForward();
+        fishAnimation.AnimateFishForward();
+    }
+
+    private void OnReleaseButtonSouth(CallbackContext ctx){
+        fishAnimation.AnimateFishIdle();
     }
 
     private void OnMoveLeftStick(CallbackContext ctx)
