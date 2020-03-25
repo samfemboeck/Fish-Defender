5f969512f745cd8bed4cebe6b5676c945a6422b8
Raffael Moser <rmoser.mmt-b2019@fh-salzburg.ac.at>
2020-03-25 10:11:27 +0100

    Add basic AudioManager

---
+++ Assets/Audio/AudioManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    private void Awake()
+    {
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.Setup();
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound sound = System.Array.Find(sounds, s => s.name == name);
+        if (sound == null)
+            Debug.LogWarning("Sound " + name + " not found!");
+        sound.Play();
+    }
+}
---
+++ Assets/Audio/Sound.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume = 1f;
+    [Range(.1f, 3f)]
+    public float pitch = 1f;
+
+    public bool looped = false;
+
+    [HideInInspector]
+    public AudioSource source;
+
+    public Sound(AudioClip clip, string name, GameObject parent, float volume = 1f, float pitch = 1f)
+    {
+        this.clip = clip;
+        this.name = name;
+
+        if (volume < 0 || volume > 1)
+            this.volume = 1f;
+        else
+            this.volume = volume;
+
+        if (pitch < .1f || pitch > 3f)
+            this.pitch = 1f;
+        else
+            this.pitch = pitch;
+
+        source = parent.AddComponent<AudioSource>();
+
+        Setup();
+    }
+
+    public void Setup()
+    {
+        source.clip = clip;
+        source.volume = volume;
+        source.pitch = pitch;
+        source.loop = looped;
+    }
+
+    public void Play()
+    {
+        source.Play();
+    }
+}
