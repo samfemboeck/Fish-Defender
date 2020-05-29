df5643b50c2e1e1970b61b50204dcf04b3539c49
RaMo_teamhellep <raf.mos@hotmail.com>
2020-05-29 12:10:23 +0200

    Implement butler push
    - Automatically pushes build to itch.io
    - Only windows at the moment

---
+++ Assets/Editor/ButlerPush.cs
@@ -0,0 +1,132 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using System.Diagnostics;
+using System.IO;
+
+public class BuildGame : MonoBehaviour
+{
+    /// <summary>
+    /// Builds all versions and pushes them to itch.io.
+    /// </summary>
+    [MenuItem("Build/Butler Push")]
+    public static void ButlerPush()
+    {
+        string[] butlerArguments = new string[6];
+
+        butlerArguments[0] = "ramo-teamhellep";     // Account
+        butlerArguments[1] = "test-01";             // Project
+
+        butlerArguments[3] = "Release";             // Configuration
+        butlerArguments[4] = "";                    // Solution directory
+
+        if (Build("Windows", BuildTarget.StandaloneWindows, butlerArguments[1], ".exe", butlerArguments[3]))
+        {
+            // Set arguments
+            butlerArguments[2] = "fd-windows-test";
+            butlerArguments[5] = "bin/Windows/Release/";
+
+            // call butler push
+            Process butler = new Process();
+            butler.StartInfo.FileName = "ButlerPush.bat";
+            butler.StartInfo.Arguments = CreateArguments(butlerArguments);
+            butler.StartInfo.UseShellExecute = false;
+            butler.StartInfo.RedirectStandardOutput = true;
+            butler.Start();
+
+            string output = butler.StandardOutput.ReadToEnd();
+            UnityEngine.Debug.Log(output);
+        }
+    }
+
+    #region Targets
+    [MenuItem("Build/Local Windows")]
+    public static void BuildWindows()
+    {
+        Build("Windows", BuildTarget.StandaloneWindows, "windowsBuild", ".exe", "Local");
+    }
+
+    [MenuItem("Build/Local Linux")]
+    public static void BuildLinux()
+    {
+        Build("Linux", BuildTarget.StandaloneLinux64, "linuxBuild", "", "Local");
+    }
+
+    [MenuItem("Build/Local OSX")]
+    public static void BuildOSX()
+    {
+        Build("IOS", BuildTarget.StandaloneOSX, "osxBuild", ".app", "Local");
+    }
+    #endregion
+
+    #region Build
+    /// <summary>
+    /// Builds a player targeting the given system.
+    /// </summary>
+    /// <param name="targetName">Name of the target folder.</param>
+    /// <param name="targetOS">Targeted system.</param>
+    /// <returns></returns>
+    private static bool Build(string folderName, BuildTarget targetOS, string executableName, string suffix, string configurationName)
+    {
+        /*
+        string path             = $"bin/{folderName}/";
+        string pathRelease      = path + "Release";
+        string pathBackup       = path + "Backup";
+        string pathExecutable   = pathRelease + "/" + executableName;
+        //*/
+
+        string path = $"bin/{folderName}/";
+        string pathConfig = path + configurationName;
+        string pathExecutable = pathConfig + "/" + executableName + suffix;
+        
+        string pathBackup = path + "Backup";
+
+        if (Directory.Exists(pathConfig))
+        {
+            if (configurationName == "Release")
+            {
+                if (Directory.Exists(pathBackup))
+                    Directory.Delete(pathBackup, true);
+                Directory.Move(pathConfig, pathBackup);
+            }
+            else
+            {
+                Directory.Delete(pathConfig, true);
+            }
+        }
+
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Scene/Leveldesign.unity" };
+        buildPlayerOptions.locationPathName = pathExecutable;
+        buildPlayerOptions.target = targetOS;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            UnityEngine.Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+            return true;
+        }
+        else if (summary.result == BuildResult.Failed)
+        {
+            UnityEngine.Debug.Log("Build failed");
+        }
+        return false;
+    }
+    #endregion
+
+    #region Utility
+    private static string CreateArguments(string[] arguments)
+    {
+        if (arguments.Length < 1) return null;
+        string output = "";
+        foreach (string arg in arguments)
+        {
+            output = output + arg + " ";
+        }
+        return output;
+    }
+    #endregion
+}
---
+++ ButlerPush.bat
@@ -0,0 +1,53 @@
+@echo off
+
+REM Butler Push
+REM Add to post build event of project to automatically zip and push the build to itch.io
+REM Needs a 7z.exe at the root of the repo
+REM Also requires the itch.io Butler to be added to PATH
+REM Events: post build event -> check master branch -> zip release folder -> push to itch
+
+
+goto comment
+rem Add this to the post-build event of your project
+
+@echo off
+
+set account=account-name
+set project=project-name
+set channel=channel-name
+
+set configuration=$(ConfigurationName)
+set solutionDir=$(SolutionDir)
+set targetDir=$(TargetDir)
+
+call %solutionDir%ButlerPush.bat %account% %project% %channel% %configuration% %solutionDir% %targetDir%
+
+:comment
+
+
+set account=%1
+set project=%2
+set channel=%3
+
+set configuration=%4
+set solutionDir=%5
+set targetDir=%6
+
+echo account: %account%
+echo project: %project%
+echo channel: %channel%
+
+echo config: %configuration%
+echo solution: %solutionDir%
+echo target: %targetDir%
+
+git branch --show-current | find /v "" | findstr /r /c:"^master$" > nul & if errorlevel 1 (
+    echo "not on master"
+) else (
+    echo "on master"
+    if %configuration% == Release (
+        echo "pushing"
+        %solutionDir%7z.exe a %targetDir%..\%project%.zip %targetDir%*
+        butler.exe push %targetDir%..\%project%.zip %account%/%project%:%channel%
+    )
+)
